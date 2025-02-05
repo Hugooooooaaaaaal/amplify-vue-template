c9245ba3cc4857b28a1b68e442065b2f1d67b085
Hugooooooaaaaaal <132125555+Hugooooooaaaaaal@users.noreply.github.com>
2025-02-05 14:02:48 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import Todos from "./components/Todos.vue";
-import Node from "./components/Node.vue";
 
 import { Authenticator } from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
@@ -10,7 +9,7 @@ import "@aws-amplify/ui-vue/styles.css";
   <main>
     <authenticator>
       <template v-slot="{ signOut }">
-        <Node />
+        <Todos />
         <button @click="signOut">Sign Out</button>
       </template>
     </authenticator>
